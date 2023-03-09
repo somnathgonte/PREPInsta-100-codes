eb449bf8e509a1ed89729d4639917deb20242246
Somnath Gonte <105125522+somnathgonte@users.noreply.github.com>
2023-03-10 00:09:00 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -1,3 +1,13 @@
+/*
+Check if the year variable is divisible by 400.
+Check if the year variable is divisible by 4 but not by 100.
+If the above conditions are satisfied, print it’s a Leap year. It’s not a Leap Year otherwise.
+
+Example
+Input : 2100
+Output: Not Leap Year
+*/
+
 import java.lang.*;
 import java.util.*;
 
@@ -16,14 +26,14 @@ public class LeapYear
         for(int year=startYear; year<=endYear; year++)
         {
 
-            if((year%4==0 && year%100!=0)||(year%400==0))
-            {
-                System.out.println(year+" is leap year");
-            }
-            else
-            {
-                System.out.println(year+" is a not leap year");
-            }
+          if((year%4==0 && year%100!=0)||(year%400==0))
+          {
+             System.out.println(year+" is leap year");
+          }
+          else
+          {
+             System.out.println(year+" is a not leap year");
+          }
        }  
     }
 }
